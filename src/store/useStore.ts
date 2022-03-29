20850f4d3fe28c95fd4149ed1ce4803b0f17e554
Simon-Brady <sbrady@consumer-edge.com>
2022-03-29 12:14:01 -0400

    chore: fix build

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,21 +1,20 @@
-import create from 'zustand';
-import symbols_staging from '../fixtures/symbols_staging.json';
+import create from "zustand";
+import symbols_staging from "../fixtures/symbols_staging.json";
 
 type TSymbol = {
-    symbol: string
-}
+  symbol: string;
+};
 
 interface IState {
-    symbols: TSymbol[],
-    selectedSymbol: null | string,
-    setSelectedSymbol: (value: string) => void
+  symbols: TSymbol[];
+  selectedSymbol: null | string;
+  setSelectedSymbol: (value: string) => void;
 }
 
-const useStore = create<IState>(set => ({
-    symbols: symbols_staging,
-    selectedSymbol: null,
-    setSelectedSymbol: (value) => set({selectedSymbol: value})
+const useStore = create<IState>((set) => ({
+  symbols: symbols_staging,
+  selectedSymbol: null,
+  setSelectedSymbol: (value) => set({ selectedSymbol: value }),
 }));
-
 
 export default useStore;
